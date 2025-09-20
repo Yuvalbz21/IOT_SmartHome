a9ef9b9ce7b9a7543fb8f1ad78b4b1fd20670499
Yuval Benzaquen <yuvalbenza10@gmail.com>
2025-09-20 20:13:57 +0300

    IOT

--- test_mqtt.py
+++
@@ -1,7 +0,0 @@
-import paho.mqtt.publish as publish
-
-BROKER = "broker.emqx.io"
-TOPIC = "smarthome/actuator/button"
-
-publish.single(TOPIC, '{"command": "toggle"}', hostname=BROKER)
-print(f"📤 Sent: 'toggle' to {TOPIC}")
